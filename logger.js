a03dcce57ffcc764d2e5d30e5b6a5f61fde37eb0
Diogo Dourado <dcdourado@gmail.com>
2021-04-18 19:56:14 -0300

    chore: add more options to Logger

--- logger.js
+++ logger.js
@@ -1,4 +1,9 @@
 const Logger = {
-  info: (msg) => console.info(msg),
-  warn: (msg) => console.warn(msg),
+  info: (msg) => console.info(`[${time}] ${msg}`),
+  log: (data) => console.log(`[${time}]`, data),
+  warn: (msg) => console.warn(`[${time}] ${msg}`),
+  error: (msg) => console.error(`[${time}] ${msg}`),
+  table: (data) => console.table(data),
 };
+
+const time = () => new Date().toLocaleTimeString();
